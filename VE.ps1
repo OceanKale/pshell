856a25310c9ec2c40c99c880df095ada6a6d5943
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-06-17 10:03:29 -0500

    Update VE.ps1

--- VE.ps1
+++ VE.ps1
@@ -1,13 +1,14 @@
-## Vy
+## Initializes synth, then proceeds to repeat indefinitely while writing $word ad infinitum (can also have it open picture[s] indefinitely)
 
 add-type -assemblyname system.speech 
 $x = 1
+$word = oily
 $wshell = new-object -comobject wscript.shell;
 $synth = new-object -typename system.speech.synthesis.speechsynthesizer
-$synth.rate = 7
+$synth.rate = 7 ### repeat rate
 do{
-	write-host -nonewline "oily"
-	$synth.speak("oily")
-	#invoke-item "c:\documents and settings\dchavez\desktop\corncorn.png"
+	write-host -nonewline $word
+	$synth.speak($word)
+	#invoke-item "corncorn.png" ### opens image file located at "path\to\image.ext"
 	
-} while($x -lt 4)
+} while($x -lt 2)
