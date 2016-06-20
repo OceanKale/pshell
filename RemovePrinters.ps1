f3846cf8354fef3bc475182b5ad11b59a8cd4320
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-06-20 12:44:17 -0500

    Update RemovePrinters.ps1

--- RemovePrinters.ps1
+++ RemovePrinters.ps1
@@ -1,11 +1,7 @@
 ## RemoveNetworkPrinters
+## removes printers with names "like" $printer
 
 $computer = $env:computername
+$printer = "\\*rint102*\*ill-10*"
 
-try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like "\\*rint102*\*ill-10*"} | foreach{$_.delete()}} catch{}
-try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like "\\*rint102*\*linic-10*"} | foreach{$_.delete()}} catch{}
-try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like "\\*rint102*\*xecutive-*"} | foreach{$_.delete()}} catch{}
-try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like "\\*rint102*\first floor finance"} | foreach{$_.delete()}} catch{}
-try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like "\\*rint102*\first floor purchasing"} | foreach{$_.delete()}} catch{}
-try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like "\\*rint102*\first floor distribution"} | foreach{$_.delete()}} catch{}
-try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like "\\*rint102*\first floor main"} | foreach{$_.delete()}} catch{}
+try{Get-WmiObject -class Win32_Printer -computer $computer | where{$_.name -like $printer} | foreach{$_.delete()}} catch{}
