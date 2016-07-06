5a4195858c869c1106e2a279b2fec7a30330864f
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-07-06 15:27:08 -0500

    Update regex file edit.ps1

--- regex file edit.ps1
+++ regex file edit.ps1
@@ -1,6 +1,8 @@
 
 ### select-string c:\dir\ -pattern "corn" (to non-destructively search for pattern)
 $FileDir = "c:\users\corn\test\*.txt" ### edits ALL text files in the directory
+$ToBeReplaced = "\*BK:" ## need to escape wildcard
+$ReplaceWith = "PIZZA"
 
 
-(get-content -path "c:\users\corm\desktop\test\*.837") | foreach-object {$_ -replace "\*BK:", "PIZZA"} | set-content -path "c:\users\corm\desktop\test\*.837"
+(get-content -path $FileDir) | foreach-object {$_ -replace "\*BK:", "PIZZA"} | set-content -path $FileDir
