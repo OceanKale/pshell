1d4e185a16ed999113f6ee031cefefd5e41fdd98
whisker-rebellion <whisker-rebellion@users.noreply.github.com>
2016-07-13 08:10:41 -0500

    Update regex file edit.ps1

--- regex file edit.ps1
+++ regex file edit.ps1
@@ -8,4 +8,10 @@ $ToBeReplaced = "\*BK:" ## need to escape wildcard
 $ReplaceWith = "PIZZA"
 
 
-(get-content -path $FileDir) | foreach-object {$_ -replace $ToBeReplaced, $ReplaceWith} | set-content -path $FileDir
+get-content -path $FileDir | foreach-object {$_ -replace $ToBeReplaced, $ReplaceWith} | set-content -path $FileDir
+
+### we can expand it with the below; obv don't run all at once, can potentially be a lot of data and this is inefficient getting/setting
+
+get-content -path "c:\users\corm\desktop\test\*.837" | foreach-object {$_ -replace "`", ""} | set-content -path "c:\users\corm\desktop\test\*.837"
+get-content -path "c:\users\corm\desktop\test\*.837" | foreach-object {$_ -replace "CAS\*CO\*45\*0", ""} | set-content -path "c:\users\corm\desktop\test\*.837" ### need to decrement line count after, so need to count lines
+select-string "c:\users\corm\desktop\test\*.837" -pattern ":UNK~"
